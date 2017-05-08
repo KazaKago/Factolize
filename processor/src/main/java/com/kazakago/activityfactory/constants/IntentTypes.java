87d27f9eeecca0439ff98b42eb13f1ec2f9b51ba
KazaKago <weathercock_perch@yahoo.co.jp>
2017-05-09 01:54:55 +0900

    cast field type when restore bundle.

--- processor/src/main/java/com/kazakago/activityfactory/constants/IntentTypes.java
+++ processor/src/main/java/com/kazakago/activityfactory/constants/IntentTypes.java
@@ -1,7 +1,6 @@
 package com.kazakago.activityfactory.constants;
 
 import com.kazakago.activityfactory.utils.TypeUtils;
-import com.squareup.javapoet.AnnotationSpec;
 import com.squareup.javapoet.TypeName;
 
 import javax.annotation.processing.ProcessingEnvironment;
@@ -13,57 +12,59 @@ import javax.lang.model.type.TypeMirror;
 public enum IntentTypes {
 
     //ArrayList<Class>
-    IntegerArrayList(Types.IntegerArrayList, "putIntegerArrayListExtra"),
-    StringArrayList(Types.StringArrayList, "putStringArrayListExtra"),
-    CharSequenceArrayList(Types.CharSequenceArrayList, "putCharSequenceArrayListExtra"),
+    IntegerArrayList(Types.IntegerArrayList, "putIntegerArrayListExtra", "getIntegerArrayListExtra", null),
+    StringArrayList(Types.StringArrayList, "putStringArrayListExtra", "getStringArrayListExtra", null),
+    CharSequenceArrayList(Types.CharSequenceArrayList, "putCharSequenceArrayListExtra", "getCharSequenceArrayListExtra", null),
 
     //ArrayList<Interface>
-    ParcelableArrayList(Types.ParcelableArrayList, "putParcelableArrayListExtra"),
+    ParcelableArrayList(Types.ParcelableArrayList, "putParcelableArrayListExtra", "getParcelableArrayListExtra", null),
 
     //Class[]
-    StringArray(Types.StringArray, "putExtra"),
-    CharSequenceArray(Types.CharSequenceArray, "putExtra"),
+    StringArray(Types.StringArray, "putExtra", "getStringArrayExtra", null),
+    CharSequenceArray(Types.CharSequenceArray, "putExtra", "getCharSequenceArrayExtra", null),
 
     //Interface[]
-    ParcelableArray(Types.ParcelableArray, "putExtra"),
+    ParcelableArray(Types.ParcelableArray, "putExtra", "getParcelableArrayExtra", null),
 
     //Class
-    String(Types.String, "putExtra"),
-    CharSequence(Types.CharSequence, "putExtra"),
-    Bundle(Types.Bundle, "putExtra"),
+    String(Types.String, "putExtra", "getStringExtra", null),
+    CharSequence(Types.CharSequence, "putExtra", "getCharSequenceExtra", null),
+    Bundle(Types.Bundle, "putExtra", "getBundleExtra", null),
 
     //Primitive[]
-    CharArray(Types.CharArray, "putExtra"),
-    ByteArray(Types.ByteArray, "putExtra"),
-    ShortArray(Types.ShortArray, "putExtra"),
-    IntArray(Types.IntArray, "putExtra"),
-    LongArray(Types.LongArray, "putExtra"),
-    FloatArray(Types.FloatArray, "putExtra"),
-    DoubleArray(Types.DoubleArray, "putExtra"),
-    BooleanArray(Types.BooleanArray, "putExtra"),
+    CharArray(Types.CharArray, "putExtra", "getCharArrayExtra", null),
+    ByteArray(Types.ByteArray, "putExtra", "getByteArrayExtra", null),
+    ShortArray(Types.ShortArray, "putExtra", "getShortArrayExtra", null),
+    IntArray(Types.IntArray, "putExtra", "getIntArrayExtra", null),
+    LongArray(Types.LongArray, "putExtra", "getLongArrayExtra", null),
+    FloatArray(Types.FloatArray, "putExtra", "getFloatArrayExtra", null),
+    DoubleArray(Types.DoubleArray, "putExtra", "getDoubleArrayExtra", null),
+    BooleanArray(Types.BooleanArray, "putExtra", "getBooleanArrayExtra", null),
 
     //Primitive
-    Char(Types.Char, "putExtra"),
-    Byte(Types.Byte, "putExtra"),
-    Short(Types.Short, "putExtra"),
-    Int(Types.Int, "putExtra"),
-    Long(Types.Long, "putExtra"),
-    Float(Types.Float, "putExtra"),
-    Double(Types.Double, "putExtra"),
-    Boolean(Types.Boolean, "putExtra"),
+    Char(Types.Char, "putExtra", "getCharExtra", "(char) 0"),
+    Byte(Types.Byte, "putExtra", "getByteExtra", "(byte) 0"),
+    Short(Types.Short, "putExtra", "getShortExtra", "(short) 0"),
+    Int(Types.Int, "putExtra", "getIntExtra", "0"),
+    Long(Types.Long, "putExtra", "getLongExtra", "0"),
+    Float(Types.Float, "putExtra", "getFloatExtra", "0"),
+    Double(Types.Double, "putExtra", "getDoubleExtra", "0"),
+    Boolean(Types.Boolean, "putExtra", "getBooleanExtra", "false"),
 
     //Interface
-    Serializable(Types.Serializable, "putExtra"),
-    Parcelable(Types.Parcelable, "putExtra");
+    Parcelable(Types.Parcelable, "putExtra", "getParcelableExtra", null),
+    Serializable(Types.Serializable, "putExtra", "getSerializableExtra", null);
 
     public TypeName typeName;
     public String putMethodName;
-    public AnnotationSpec[] annotations;
+    public String getMethodName;
+    public String getDefaultValue;
 
-    IntentTypes(TypeName typeName, String putMethodName, AnnotationSpec... annotations) {
+    IntentTypes(TypeName typeName, String putMethodName, String getMethodName, String getDefaultValue) {
         this.typeName = typeName;
         this.putMethodName = putMethodName;
-        this.annotations = annotations;
+        this.getMethodName = getMethodName;
+        this.getDefaultValue = getDefaultValue;
     }
 
     public static IntentTypes resolve(ProcessingEnvironment processingEnv, TypeMirror typeMirror) {
